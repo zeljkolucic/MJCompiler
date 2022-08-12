91f2d80e4988d89b2f22752d82518eeff7742dc3
Zeljko Lucic <zeljko.lucic99@gmail.com>
2022-08-13 01:30:31 +0200

    Implement action for Type syntax node

--- src/rs/ac/bg/etf/pp1/SemanticPass.java
+++ src/rs/ac/bg/etf/pp1/SemanticPass.java
@@ -10,6 +10,27 @@ public class SemanticPass extends VisitorAdaptor {
 
 	Logger log = Logger.getLogger(getClass());
 	
+	public void report_error(String message, SyntaxNode info) {
+		errorDetected = true;
+		StringBuilder msg = new StringBuilder(message);
+		int line = (info == null) ? 0 : info.getLine();
+		if (line != 0)
+			msg.append(" na liniji ").append(line);
+		log.error(msg.toString());
+		System.err.println(msg.toString());
+	}
+
+	public void report_info(String message, SyntaxNode info) {
+		StringBuilder msg = new StringBuilder(message);
+		int line = (info == null) ? 0 : info.getLine();
+		if (line != 0)
+			msg.append(" na liniji ").append(line);
+		log.info(msg.toString());
+		System.out.println(msg.toString());
+	}
+	
+	boolean errorDetected = false;
+	
 	/**
 	 * Dodaje objektni cvor u tabelu simbola
 	 * i cuva referencu na njega. Otvara novi opseg
@@ -24,7 +45,6 @@ public class SemanticPass extends VisitorAdaptor {
 		Tab.openScope();
 	}
 	
-	
 	/**
 	 * Uvezuje lokalne simbole koji pripadaju
 	 * tekucem opsegu i postavlja polje `locals`
@@ -35,6 +55,33 @@ public class SemanticPass extends VisitorAdaptor {
 		ProgName progName = program.getProgName();
 		Tab.chainLocalSymbols(progName.obj);
 		Tab.closeScope();
+	}
+	
+	/**
+	 * Ispituje da li se dati tip vec nalazi u tabeli
+	 * simbola, i ukoliko se ne nalazi to znaci da 
+	 * nije validan tip. Ukoliko se nalazi, a tip 
+	 * tog cvora nije Type, to znaci da je u nekom 
+	 * ugnjezdenijem opsegu korisnik koristio kljucnu 
+	 * rijec za naziv promjenljive.
+	 */
+	public void visit(Type type) {
+		String typeName = type.getTypeName();
+		Obj typeNode = Tab.find(typeName);
+		
+		if(typeNode == Tab.noObj) {
+			errorDetected = true;
+			type.struct = Tab.noType;
+			report_error("Nije pronadjen tip " + typeName + " u tabeli simbola!", null);
+		} else {
+			if (typeNode.getKind() == Obj.Type) {
+				type.struct = typeNode.getType();
+			} else {
+				errorDetected = true;
+				type.struct = Tab.noType;
+				report_error("Greska: Ime " + type.getTypeName() + " ne predstavlja tip!", type);
+			}
+		}
 	}
 	
 }
--- test/rs/ac/bg/etf/pp1/MJParserTest.java
+++ test/rs/ac/bg/etf/pp1/MJParserTest.java
@@ -14,6 +14,7 @@ import org.apache.log4j.xml.DOMConfigurator;
 
 import rs.ac.bg.etf.pp1.ast.Program;
 import rs.ac.bg.etf.pp1.util.Log4JUtils;
+import rs.etf.pp1.symboltable.Tab;
 
 public class MJParserTest {
 
@@ -38,14 +39,16 @@ public class MJParserTest {
 	        Symbol s = p.parse();  //pocetak parsiranja
 	        
 	        Program prog = (Program)(s.value); 
+	        Tab.init();
 			// ispis sintaksnog stabla
 			log.info(prog.toString(""));
-			log.info("===================================");
+			log.info("===================================");					
 
 			// ispis prepoznatih programskih konstrukcija
-			RuleVisitor v = new RuleVisitor();
+			SemanticPass v = new SemanticPass();
 			prog.traverseBottomUp(v); 
 			
+			Tab.dump();
 		} 
 		finally {
 			if (br != null) try { br.close(); } catch (IOException e1) { log.error(e1.getMessage(), e1); }
